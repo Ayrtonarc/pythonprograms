963ddd74ddd199f85b83d04a52a264642b86346f
Ayrton Santos <ayrton.arc@gmail.com>
2025-12-02 18:30:24 -0800

    programa 3

--- 3_programa.py
+++ 3_programa.py
@@ -6,4 +6,14 @@ def pedir_texto():
 def es_palindromo(texto):
     texto_limpio = ''.join(texto.split()).lower()
     return texto_limpio == texto_limpio[::-1]
+def mostrar_resultado(texto, es_palindromo):
+    if es_palindromo:
+        print(f'El texto "{texto}" es un palíndromo.')
+    else:
+        print(f'El texto "{texto}" no es un palíndromo.')
 
+
+if __name__ == "__main__":
+    texto = pedir_texto()
+    resultado = es_palindromo(texto)
+    mostrar_resultado(texto, resultado)
